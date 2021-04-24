d9aa6e6935f3ac323fad7b7b90ddf0dd420d13e5
zyansheep <zyansheep@github.com>
2021-04-24 15:15:39 -0400

    improve camera

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
-#![allow(dead_code)]
-use bevy::{prelude::*, render::camera::Camera};
+#![allow(unused_imports, unused_import_braces, dead_code)]
+use bevy::{diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin}, input::mouse::MouseMotion, prelude::*, render::camera::Camera};
 
 mod player;
 use player::{Player};
+mod camera;
+use camera::{OrbitCameraPlugin, CameraFocusEvent, CameraInterpolation};
 
 fn main() {
 	App::build()
-		.insert_resource(Msaa { samples: 8 })
-		.add_plugins(DefaultPlugins)
-		.add_startup_system(setup.system())
-    	.add_system(keyboard_input_system.system())
-    	.add_system(camera_movement_system.system())
-		.run();
+	.insert_resource(Msaa { samples: 8 })
+	.add_plugins(DefaultPlugins)
+	.add_plugin(OrbitCameraPlugin)
+	.add_startup_system(setup.system())
+	.add_system(player_movement.system())
+	//.add_system(camera_movement_system.system())
+	.run();
 }
 
 /// set up a simple 3D scene
@@ -47,31 +50,41 @@ fn setup(
 	});
 }
 
-/// This system prints 'A' key state
-fn keyboard_input_system (
+fn player_movement(
+	mut focus_broadcast: EventWriter<CameraFocusEvent>,
 	keyboard_input: Res<Input<KeyCode>>,
 	mut player_transform: Query<&mut Transform, With<Player>>
 ) {
 	let mut player_transform = player_transform.single_mut().unwrap();
 	
+	let mut moved = false;
 	if keyboard_input.pressed(KeyCode::W) {
 		//info!("'W' currently pressed");
+		moved = true;
 		player_transform.translation.x += 0.1; 
 	}
 	if keyboard_input.pressed(KeyCode::S) {
 		//info!("'S' currently pressed");
+		moved = true;
 		player_transform.translation.x -= 0.1;
 	}
 	if keyboard_input.pressed(KeyCode::A) {
 		//info!("'A' currently pressed");
+		moved = true;
 		player_transform.translation.z -= 0.1;
 	}
 	if keyboard_input.pressed(KeyCode::D) {
 		//info!("'A' currently pressed");
+		moved = true;
 		player_transform.translation.z += 0.1;
 	}
+	if moved {
+		focus_broadcast.send(CameraFocusEvent::new(player_transform.translation,CameraInterpolation::None) );
+	}
 }
-fn camera_movement_system (
+
+/* fn camera_movement_system(
+	mut mouse_motion_events: EventReader<MouseMotion>,
 	mut q: QuerySet<(
 		Query<&Transform, With<Player>>,
 		Query<&mut Transform, With<Camera>>
@@ -81,4 +94,4 @@ fn camera_movement_system (
 	let mut camera_transform = q.q1_mut().single_mut().unwrap();
 
 	camera_transform.look_at(player_transform.translation, Vec3::new(0.,1.,0.));
-}
+} */
